7ba192cc3fb5d78c4bcad7634a635414287a768f
jackkower <jackkower@gmail.com>
2020-08-05 22:42:49 -0700

    initial commit

---
+++ build/precache-manifest.071072e4f33d0bd29f257ef62bfde231.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61988ed67f6297bd13dfd44a6ff9e8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc25b1559b38372a998",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c7ed08c6347d5bd9de1",
+    "url": "/static/css/main.a0585de0.chunk.css"
+  },
+  {
+    "revision": "6dc25b1559b38372a998",
+    "url": "/static/js/2.9d74bb51.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.9d74bb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7ed08c6347d5bd9de1",
+    "url": "/static/js/main.b287fe45.chunk.js"
+  },
+  {
+    "revision": "bb495e144ded23ad8226",
+    "url": "/static/js/runtime-main.f1bc55ce.js"
+  },
+  {
+    "revision": "b57bcb6cd1b5694d08b46619c42f92f4",
+    "url": "/static/media/SD.b57bcb6c.jpg"
+  },
+  {
+    "revision": "7d76f88dc3f5fe5b326c3d255a0a6faa",
+    "url": "/static/media/chig.7d76f88d.jpg"
+  },
+  {
+    "revision": "47d2bbf1f4867982182195d47d560a10",
+    "url": "/static/media/hipster.47d2bbf1.jpg"
+  },
+  {
+    "revision": "ef78ff75f07ab569af70185a411549b6",
+    "url": "/static/media/jackrec.ef78ff75.jpg"
+  },
+  {
+    "revision": "de9628846cba038b8f9ac1c003a86425",
+    "url": "/static/media/nyc.de962884.jpg"
+  },
+  {
+    "revision": "f50286185fe19d7ef937b27612ad6a7d",
+    "url": "/static/media/testi.f5028618.JPG"
+  }
+]);
