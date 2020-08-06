a5ecbf8a125bb5e53f059e8ff1c49efc3abce039
Jaren Lynch <jaren.d.lynch@gmail.com>
2020-08-05 23:49:26 -0700

    Updates

---
+++ precache-manifest.432477c35d419bb305bd7f321b3fcdbd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd2ab548e341882db880e085fc30c06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "98c885723265ccdbf3d6",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0a46fb4f2b93bfcb206",
+    "url": "./static/css/main.0a300fcc.chunk.css"
+  },
+  {
+    "revision": "98c885723265ccdbf3d6",
+    "url": "./static/js/2.9ff2e3d0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.9ff2e3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a46fb4f2b93bfcb206",
+    "url": "./static/js/main.4314b759.chunk.js"
+  },
+  {
+    "revision": "49bd2ecaec064a0f08e4",
+    "url": "./static/js/runtime-main.2d7a8eb9.js"
+  },
+  {
+    "revision": "b57bcb6cd1b5694d08b46619c42f92f4",
+    "url": "./static/media/SD.b57bcb6c.jpg"
+  },
+  {
+    "revision": "7d76f88dc3f5fe5b326c3d255a0a6faa",
+    "url": "./static/media/chig.7d76f88d.jpg"
+  },
+  {
+    "revision": "47d2bbf1f4867982182195d47d560a10",
+    "url": "./static/media/hipster.47d2bbf1.jpg"
+  },
+  {
+    "revision": "ef78ff75f07ab569af70185a411549b6",
+    "url": "./static/media/jackrec.ef78ff75.jpg"
+  },
+  {
+    "revision": "de9628846cba038b8f9ac1c003a86425",
+    "url": "./static/media/nyc.de962884.jpg"
+  },
+  {
+    "revision": "f50286185fe19d7ef937b27612ad6a7d",
+    "url": "./static/media/testi.f5028618.JPG"
+  }
+]);
